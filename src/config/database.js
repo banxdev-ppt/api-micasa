b04f4b5dc6ec4b214086609b406c4a1c5ab858d0
ponpitak <ponpitak@ghugcode.co.th>
2025-01-14 22:40:55 +0700

    config => database config เพิ่มการใช้งาน dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "snp67_micasa",
-  port: Number(process.env.DB_PORT) || 3306,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_DATABASE || 'snp67_micasa',
+  port: Number(process.env.DB_PORT) || 3307,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,

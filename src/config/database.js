2399b3cf949eda46b1df06187984825d4c1994b6
Plaimee <plaimee.tunayip@gmail.com>
2025-02-19 17:28:00 +0700

    try deploy on server

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,17 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'snp67_micasa',
-  port: Number(process.env.DB_PORT) || 3307,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: { rejectUnauthorized: true },
 });
 
 module.exports = { db };
